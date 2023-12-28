233020eeddaeb826d408bef449035b3186ceec13
Alexander Samushenkov <samushenkov@live.com>
2023-12-28 12:35:07 +0300

    Cancel long polling when user stops service

--- src/HunsterService/Controllers/HunsterController.cs
+++ src/HunsterService/Controllers/HunsterController.cs
@@ -12,16 +12,20 @@ namespace HunsterService.Controllers
     {
         private readonly IMatchProvider _matchProvider;
         private readonly IMatchEmitter _matchEmitter;
+        private readonly IHostApplicationLifetime _applicationLifetime;
 
-        public HunsterController(IMatchProvider missionBagProvider, IMatchEmitter matchEmitter)
+        public HunsterController(IMatchProvider missionBagProvider, IMatchEmitter matchEmitter, IHostApplicationLifetime applicationLifetime)
         {
             _matchProvider = missionBagProvider;
             _matchEmitter = matchEmitter;
+            _applicationLifetime = applicationLifetime;
         }
 
         [HttpGet("{version}")]
         public async Task<Match> GetLastMatch(string version, CancellationToken token)
         {
+            using var tokenSource = CancellationTokenSource.CreateLinkedTokenSource(token, _applicationLifetime.ApplicationStopping);
+
             var matchSource = new TaskCompletionSource<Match>(TaskCreationOptions.RunContinuationsAsynchronously);
             var matchSourceUpdater = matchSource.SetResult;
 
@@ -29,7 +33,7 @@ namespace HunsterService.Controllers
 
             try
             {
-                var match = await _matchProvider.GetLastMatchAsync(token);
+                var match = await _matchProvider.GetLastMatchAsync(tokenSource.Token);
 
                 if (match.MissionVersion != version)
                 {
@@ -37,7 +41,7 @@ namespace HunsterService.Controllers
                     return match;
                 }
 
-                var matchTimeoutTask = Task.Delay(TimeSpan.FromMinutes(1), token);
+                var matchTimeoutTask = Task.Delay(TimeSpan.FromMinutes(1), tokenSource.Token);
                 var matchTask = await Task.WhenAny(matchTimeoutTask, matchSource.Task);
 
                 if (matchTask != matchTimeoutTask)
